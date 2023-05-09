8009593c691b621d355f9429d92e2ebe8c6b0947
ImanGoudarzvand <im.goudarz@gmail.com>
2023-05-09 22:51:27 +0330

    business logic services added

---
+++ src/auth/app/services/user.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import Session
+from ..database import pwd_context
+from ..models import User
+
+
+def create_user(*, email, password, db: Session):
+
+    hashed_password = pwd_context.hash(password)
+    
+    db_user = User(email=email, password = hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
