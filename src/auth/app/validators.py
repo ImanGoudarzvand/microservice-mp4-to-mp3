0f0b9b560cdfcc495748649eeff32ae67c867533
ImanGoudarzvand <im.goudarz@gmail.com>
2023-05-09 22:50:07 +0330

    password validators added

---
+++ src/auth/app/validators.py
@@ -0,0 +1,24 @@
+import re 
+from fastapi import HTTPException
+
+def number_validator(password):
+    regex = re.compile('[0-9]')
+    if regex.search(password) == None:
+        raise HTTPException(400, "password must include at least a number")
+
+
+def letter_validator(password):
+    regex = re.compile('[a-zA-Z]')
+    if regex.search(password) == None:
+        raise HTTPException(400, "password must include at least a letter")
+
+
+def special_char_validator(password):
+    regex = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
+    if regex.search(password) == None:
+        raise HTTPException(400, "password must include at least a special character")
+
+
+def min_length_validator(password):
+    if len(password) < 8:
+        raise HTTPException(400, "password length must be at least 8")
