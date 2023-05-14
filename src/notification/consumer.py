13b2da75b2f460cb995324fe37199e23e9cd9eb1
ImanGoudarzvand <im.goudarz@gmail.com>
2023-05-14 23:08:29 +0330

    consume from mp3 rabbitmq queue

---
+++ src/notification/consumer.py
@@ -0,0 +1,35 @@
+import pika, os
+from send import email 
+
+def main(): 
+ 
+    # rabbitmq connection
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq-service'))
+    channel = connection.channel()
+
+    def callable(ch, method, properties, body):
+        err = email.notification(body)
+        if err:
+            ch.basic_nack(delivery_tag=method.delivery_tag)
+        else:
+            ch.basic_ack(delivery_tag = method.delivery_tag)
+             
+
+    channel.basic_consume(
+        queue = os.environ.get('MP3_QUEUE'),
+        on_message_callback = callable    
+    )
+
+    print("Waiting for messages. To exit press CTRL+C")
+
+    channel.start_consuming()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
